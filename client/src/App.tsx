ce53e4bf3baa8f5eca607099b084ad3a1cad4eb3
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-08-06 08:11:59 +0300

    presettings

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.module.scss';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="/" className="logo">
+          Blog
+        </a>
+      </header>
+    </main>
+  );
+}
+
+export default App;
