13a93109f93e77d974950ac6729db4e0bb2a12b5
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-08-09 02:47:20 +0300

    part of server-side and client-side

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
+// This line imports the Express.js framework, making it available for use in your code.
 const express = require('express');
+
+const cors = require('cors');
+
+//  This initializes an instance of the Express application. You'll use this instance to define routes, middleware, and other
+//  settings for your server.
 const app = express();
 
-app.get('/test', (req, res) => {
+app.use(cors());
+
+//  This code sets up a route for handling HTTP POST requests to the '/register' endpoint. When a POST request is made to this 
+//  endpoint, the callback function (req, res) => { ... } is executed. Inside this callback, you're sending a JSON response of 
+//  'test ok' back to the client.
+app.post('/register', (req, res) => {
     res.json('test ok');
 });
 
+// This line starts the Express server and makes it listen on port 4000. When the server starts successfully, the provided callback 
+// function is executed, which logs a message to the console indicating that the server is listening.
 app.listen(4000, () => {
     console.log('Server is listening on port 4000');
 });
--- client/src/components/Pages/RegisterPage.tsx
+++ client/src/components/Pages/RegisterPage.tsx
@@ -1,10 +1,44 @@
+import React, { useState, FormEvent } from "react";
+
 export function RegisterPage() {
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function register(ev: FormEvent<HTMLFormElement>) {
+        ev.preventDefault();
+        try {
+            const response = await fetch("http://localhost:4000/register", {
+                method: "POST",
+                body: JSON.stringify({ username, password }),
+                headers: { "Content-Type": "application/json" },
+            });
+            
+            if (response.ok) {
+                console.log("Registration successful");
+            } else {
+                console.error("Registration failed");
+            }
+        } catch (error) {
+            console.error("Error during registration:", error);
+        }
+    }
+
     return (
-        <form className="register">
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="pssword" />
-            <button>Register</button>   
+            <input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={(ev) => setUserName(ev.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+            />
+            <button type="submit">Register</button>
         </form>
-    )
+    );
 }
